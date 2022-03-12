01d143c2db25b71fd2399d36fbc2945ce21fd816
amaniq88 <amani.musallamq88@gmail.com>
2022-03-12 09:51:42 +0300

    6 tests passed

--- data_structures_and_algorithms/linked_list/Linked_List.py
+++ data_structures_and_algorithms/linked_list/Linked_List.py
@@ -1,46 +1,102 @@
-from os import curdir
-from Node import Node
+
+
+class Node:
+    '''
+    Class Node which define the node value and initial the next to None
+    '''
+    
+    def __init__(self, dataval):  
+            self.dataval = dataval
+            self.nextval = None
+       
+        
+
 
 class LinkedList:
+    '''
+    Class LinkedList which used to represent the Linked List with initial value of Head to None and it has 
+    the following methods : 
+    append which Take input Node instancetce and define the head and next 
+    insert which insert new element after the head of the node if exsist 
+    Includes which check if this elemnt is there or not 
+    '''
     def __init__(self):
       self.head = None
-    
+   
+   
+    def append(self, node):
+        '''
+        append menthod which used to append the new node to the linked list  at the end of the list and keep the last elent is null 
+        Input :  Node instance 
+        '''
+        if self.head is None:
+            self.head = node
+
+        else:
+            current = self.head
+            while current.nextval is not None:
+                current = current.nextval
+            current.nextval = node
 
     def __str__(self):
-        list = ""
-        current = self.head
-        list = f'"{ "{self.head}" } ->"'
-        while(current):
-            list += f'"{ "{current}" } ->"'
-            current = current.next
+        '''
+        this method is used to print in certain formate like 
+        {a} -> {b} -> {c} -> {d} -> NULL
         
-            return list
-
-
+        '''
+        output = ""
+        if self.head is None:
+            output = "Empty linked list"
+        else:
+            current = self.head
+            while(current):
+                output+= f' {current.dataval} ->'
+                current = current.nextval
+            
+            output+= " NULL"
+        return output
+    
+    
+    
     def insert(self, newElement):
-        newNode = Node(newElement) 
-        if(self.head == "None"):
-            newNode.next = self.head
-            self.head = newNode
-        else:    
+        '''
+        method to insert new element after the head element 
+        input : element to added in the linked list
+        '''
+        #except NameError:
+ 
+
+        if(self.head == None):
+            newNode = Node(newElement)
+            self.append(newNode)
+        else:
+            newNode = Node(newElement)     
             temp = self.head
-            newNode.next = temp.next
-            temp.next = newNode  
+            newNode.nextval = temp.nextval
+            temp.nextval = newNode
+     
+
+
      
     
     
     def includes(self , value ): 
+        '''
+        Arguments: value
+        Returns: Boolean
+        Indicates whether that value exists as a Node’s value somewhere within the list.
+        '''
         current = self.head
         # loop till current not equal to None
         while (current):
-            if current.data == value:
+            if current.dataval == value:
                 return True # data found
              
-            current = current.next
+            current = current.nextval
          
         return False # Data Not found
 
 if __name__ == '__main__':
-    list = LinkedList()
-    list.head("testA")
-    print(list)
+    ll = LinkedList()
+
+
